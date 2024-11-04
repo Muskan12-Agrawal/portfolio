761611253aef3181f7b0578288c0f1554ad233da
Tushar Bhardwaj <muskanagrawal7275@gmail.com>
2024-11-05 00:35:19 +0530

    change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="https://topmate.io/tusharbhardwaj" target="_blank">
+        <a href=mailto:Muskagrawal321@gmail.com" target="_blank">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
@@ -24,7 +24,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Tushar Bhardwaj
+          Copyright © 2024 Muskan Agrawal
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
--- data/index.ts
+++ data/index.ts
@@ -205,16 +205,11 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
-      link: "https://github.com/TuShArBhArDwA",
+      link: "https://github.com/Muskan12-Agrawal",
     },
     {
       id: 2,
-      img: "/twit.svg",
-      link: "https://twitter.com/TusharBhard2004",
-    },
-    {
-      id: 3,
       img: "/link.svg",
-      link: "https://www.linkedin.com/in/bhardwajtushar2004/",
+      link: "https://www.linkedin.com/in/muskan-agrawal-6a510326a/",
     },
   ];
